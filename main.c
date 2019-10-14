3c1132b2c61a8c901894ba435b1ee9e00cf8def3
domchiklik <rasengan644@gmail.com>
2019-10-13 20:30:47 -0500

    Fixed commit

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#define CMP strcmp
 
 int main(int *argc, char *argv[]){
 
@@ -11,42 +12,51 @@ int main(int *argc, char *argv[]){
   
   DIR *dr;
 
+  
   // FLAGS
-  //_____________________________
+  //_________________________
   if (strcmp(argv[1], "-b") == 0)
     {
       //CHANGE FILE PATH HERE
       //           VVVVVVVVVVV
       dr = opendir("/usr/bin/");
-    } else if (strcmp(argv[1], "-d") == 0)
+    } else if (CMP(argv[1], "-d") == 0)
     {
       // CHANGE FILE PATH HERE
       //           VVVVVVVVVVVVVVVVVVVVVVVVV
+<<<<<<< HEAD
+      dr = opendir("/home/aliets/Downloads/");
+      // HELP FLAG
+    } else if (CMP(argv[1], "-h") == 0)
+=======
       dr = opendir("/home/sysname/Downloads/");
     } else if (strcmp(argv[1], "-h") == 0)
+>>>>>>> 8a476dd58cd9453efb78a3b0374c4ef14cc4271a
     {
       printf("___FLAGS___\n");
       printf("-b goes to bin\n-d goes to downloads\n-h for help");
       printf("change the paths in main.c to get flags to work!");
+      // OPEN DIRECTORY YOU INPUT
     } else
     {
       dr = opendir(argv[1]);
     }
-
-  /*                           */
-  //FLAGS
-  // PRINT DIRECTORIES
+  // IF DIRECTORY IS INVALID PRINT "No directory"
   if (dr == NULL)
 	{
 	  printf("No directory");
 	  return 0; 
 	}
   while ((de = readdir(dr)) != NULL){
+    // LINE NUMBERS
     c++;
-    printf("%d. %s\n", c, de->d_name);
+    // PRINT DIRECTORY NAME
+    printf("|\033[1;35m%d. \033[1;36m*[\033[1;34m%s\033[1;36m]*\n", c, de->d_name);
+    // FORMATTING
+    if (c % 2 == 0 || c == 1) printf("\t\t\t");
   }
   
   closedir(dr);
     
   return 0;
-}
+  }
